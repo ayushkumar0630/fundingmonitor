a6ba2108a057842cc142efb5ba74e062556a654e
Ayush Kumar <ayushkumar0630@gmail.com>
2019-02-02 16:42:36 -0600

    adding master

--- fundingmonitor/src/components/master.vue
+++ fundingmonitor/src/components/master.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="hello">
-    <h1>Index Page</h1>
-    <b-alert show>Default Alert</b-alert>
-
-    <b-alert variant="success" show>Success Alert</b-alert>
-
-    <b-alert variant="danger"
-             dismissible
-             :show="showDismissibleAlert"
-             @dismissed="showDismissibleAlert=false">
-      Dismissible Alert!
-    </b-alert>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+      <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Punny headline</h1>
+        <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple's marketing pages.</p>
+        <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+      </div>
+    </div>
   </div>
 </template>
 
