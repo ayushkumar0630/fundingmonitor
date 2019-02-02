b8f6d6287294de26b16c8607ece21896f423cf3b
Ayush Kumar <ayushkumar0630@gmail.com>
2019-02-02 16:33:02 -0600

    adding frontend

--- fundingmonitor/src/App.vue
+++ fundingmonitor/src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="site-header sticky-top py-1">
+      <div class="container d-flex flex-column flex-md-row justify-content-between">
+        <a class="py-2" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+        </a>
+        <a class="py-2 d-none d-md-inline-block" href="/">Tour</a>
+        <a class="py-2 d-none d-md-inline-block" href="/">Product</a>
+        <a class="py-2 d-none d-md-inline-block" href="/">Features</a>
+        <a class="py-2 d-none d-md-inline-block" href="/">Enterprise</a>
+        <a class="py-2 d-none d-md-inline-block" href="/">Support</a>
+        <a class="py-2 d-none d-md-inline-block" href="/">Pricing</a>
+        <a class="py-2 d-none d-md-inline-block" href="/">Cart</a>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
@@ -18,6 +31,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#globalnav{
+  color: white;
+  -webkit-box-shadow: 0 8px 6px -6px #999;
+  -moz-box-shadow: 0 8px 6px -6px #999;
+  box-shadow: 0 8px 6px -6px #999;
+}
+#signin{
+  text-emphasis: bold;
+  color: red;
 }
 </style>
