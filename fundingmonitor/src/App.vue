3752f028972f6144e79d50d23dd6687211c6accf
Ayush Kumar <ayushkumar0630@gmail.com>
2019-02-02 19:18:16 -0600

    oops

--- fundingmonitor/src/App.vue
+++ fundingmonitor/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <nav class="site-header sticky-top py-1">
+    <nav class="site-header sticky-top py-1" id="globalnav">
       <div class="container d-flex flex-column flex-md-row justify-content-between">
         <a class="py-2" href="#">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
@@ -10,11 +10,26 @@
         <a class="py-2 d-none d-md-inline-block" href="/features">Features</a>
         <a class="py-2 d-none d-md-inline-block" href="/faq">FAQ</a>
         <a class="py-2 d-none d-md-inline-block" href="/contact">Contact</a>
-        <!--<a class="py-2 d-none d-md-inline-block" href="/">Pricing</a>-->
         <a class="py-2 d-none d-md-inline-block" href="/signin">Sign In</a>
       </div>
     </nav>
     <router-view/>
+    <footer class="container py-5">
+      <div class="row">
+        <div class="col-md">
+          <h4>FundingMonitor</h4>
+          <small class="d-block mb-3 text-muted">©2019</small>
+        </div>
+        <div>
+          <h5>About</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="/contact">Contact</a></li>
+            <li><a class="text-muted" href="/team">Team</a></li>
+            <li><a class="text-muted" href="/privacy">Privacy</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -33,7 +48,7 @@ export default {
   color: #2c3e50;
 }
 #globalnav{
-  color: white;
+  background-color: black;
   -webkit-box-shadow: 0 8px 6px -6px #999;
   -moz-box-shadow: 0 8px 6px -6px #999;
   box-shadow: 0 8px 6px -6px #999;
