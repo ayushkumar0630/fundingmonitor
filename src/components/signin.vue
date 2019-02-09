398129c9cd41a60163c829bdbd78b7c0039e699f
Abhishek Belani <Abhi@Abhisheks-MacBook-Pro-2.local>
2019-02-09 15:13:36 -0600

    signin page

--- src/components/signin.vue
+++ src/components/signin.vue
@@ -1,6 +1,19 @@
 <template>
  <div class="signin">
-  <h1>{{title}}</h1>
+  <form class="form-signin" id="signinform">
+      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
  </div>
 </template>
 <script>
@@ -8,10 +21,22 @@ export default {
   name: 'signin',
   data () {
     return {
-      title: 'Send me your FB credentials'
     }
   }
 }
 </script>
 <style scoped>
+#signinform{
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: 50px auto;
+}
+#inputPassword{
+  margin-bottom: 16px;
+}
+.signin{
+  background-color: #fbeed7;
+}
 </style>
