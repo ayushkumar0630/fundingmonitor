39e92d858d5a5c465543f1c16371586a6a36f106
Ayush Kumar <ayushkumar0630@gmail.com>
2019-02-13 19:54:17 -0600

    adding right routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import master from '@/components/master'
-import home from '@/components/home'
-import blog from '@/components/blog'
-import pricing from '@/components/pricing'
-import features from '@/components/features'
-import faq from '@/components/faq'
-import contact from '@/components/contact'
 import signin from '@/components/signin'
-import team from '@/components/team'
-import privacy from '@/components/privacy'
-import signup from '@/components/signup'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
@@ -25,54 +16,9 @@ export default new Router({
       component: master
     },
     {
-      path: '/home',
-      name: 'home',
-      component: home
-    },
-    {
-      path: '/blog',
-      name: 'blog',
-      component: blog
-    },
-    {
-      path: '/pricing',
-      name: 'pricing',
-      component: pricing
-    },
-    {
-      path: '/features',
-      name: 'features',
-      component: features
-    },
-    {
-      path: '/faq',
-      name: 'faq',
-      component: faq
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: contact
-    },
-    {
       path: '/signin',
       name: 'signin',
       component: signin
-    },
-    {
-      path: '/team',
-      name: 'team',
-      component: team
-    },
-    {
-      path: '/privacy',
-      name: 'privacy',
-      component: privacy
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: signup
     }
   ]
 })
