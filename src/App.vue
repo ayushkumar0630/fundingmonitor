69237830634664ac10c597ff6ffa7a7350802568
Ayush Kumar <ayushkumar0630@gmail.com>
2019-02-13 20:07:50 -0600

    working now

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-    <Navbar/>
     <router-view/>
-    <Footer/>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Navbar,
-    Footer
-  }
+  name: 'App'
 }
 </script>
 
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="dashboard">
-    
-    
     </div>
 </template>
 
