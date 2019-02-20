0bb64ea7e1865ddfd3e67b0dc8651e347bd1e906
Lahiru Chathuranga <lahiru.b@creativesoftware.com>
2019-02-20 12:38:16 +0530

    theme change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,48 @@
 <template>
-    <div class="navbar">
-        <nav>
-            <div class="nav-wrapper">
-            <a href="#" class="brand-logo">Logo <img src="../assets/fundingmonitor.png" alt=""></a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><router-link :to="{ name: 'signin'}">Sign In</router-link></li>
-            </ul>
-            </div>
-        </nav>
-    </div>
+  <div class="navbar">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">
+          <img src="../assets/fundingmonitor.svg" alt width="40px">
+          <span>Funding Monitor</span>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <router-link :to="{ name: 'signin'}">Sign In</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'navbar',
-  data () {
-    return {
-    }
+  name: "navbar",
+  data() {
+    return {};
   }
-}
+};
 </script>
 <style lang="scss" scoped>
-.nav-wrapper{
-  background-color: #013e4e;
+nav{
+  box-shadow: none;
+}
+.nav-wrapper {
+  background-color: #ffba5a;
+
+  .brand-logo {
+    img {
+      display: block;
+      float: left;
+      margin: 10px 12px;
+    }
+    span {
+      font-size: 18px;
+      display: block;
+      float: left;
+      color: #ffffff;
+    }
+  }
 }
 </style>
