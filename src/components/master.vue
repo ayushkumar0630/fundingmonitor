de6bae96051662db2fd1dd7c592aab58db384de8
Ayush Kumar <ayushkumar0630@gmail.com>
2019-03-10 17:45:45 -0500

    adding full front end

--- src/components/master.vue
+++ src/components/master.vue
@@ -121,15 +121,12 @@
                 <hr>
                 <ul class="fa-ul">
                   <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited Private Projects</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated Phone Support</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Single Company</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>1GB Limit</li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Employees</li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated Support</li>
                 </ul>
-                <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
+                <a href="#" class="btn btn-block btn-primary text-uppercase">Sign Up</a>
               </div>
             </div>
           </div>
@@ -138,19 +135,16 @@
             <div class="card mb-5 mb-lg-0">
               <div class="card-body">
                 <h5 class="card-title text-muted text-uppercase text-center">Plus</h5>
-                <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
+                <h6 class="card-price text-center">$10<span class="period">/month</span></h6>
                 <hr>
                 <ul class="fa-ul">
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>5 Users</strong></li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>50GB Storage</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>
-                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status Reports</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Single Company</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Limit</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>5 Employees</li>
+                  <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated Support</li>
                 </ul>
-                <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
+                <a href="#" class="btn btn-block btn-primary text-uppercase">Sign Up</a>
               </div>
             </div>
           </div>
@@ -159,19 +153,16 @@
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
-                <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
+                <h6 class="card-price text-center">$50<span class="period">/month</span></h6>
                 <hr>
                 <ul class="fa-ul">
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited Users</strong></li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free Subdomains</li>
-                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Multiple Companies</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>No Storage Limit</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>No Employee Limit</li>
+                  <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Support</li>
                 </ul>
-                <a href="#" class="btn btn-block btn-primary text-uppercase">Button</a>
+                <a href="#" class="btn btn-block btn-primary text-uppercase">Sign Up</a>
               </div>
             </div>
           </div>
